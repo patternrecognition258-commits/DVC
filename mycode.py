7cf4639ca52f495a38407f91928a0f59c1e7183c
patternrecognition258 <patternrecognition258@gmail.com>
2026-04-10 02:18:09 +0530

    Data V2

--- mycode.py
+++ mycode.py
@@ -1,9 +1,9 @@
 import pandas as pd 
 
 data = {
-    "name":  ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Helen", "Ian", "Jack"],
-    "age":   [25, 30, 22, 28, 26, 24, 27, 23, 29, 21],
-    "score": [85, 78, 92, 88, 95, 80, 89, 76, 84, 90]
+    "name":  ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Helen", "Ian", "Jack","Sam"],
+    "age":   [25, 30, 22, 28, 26, 24, 27, 23, 29, 21,23],
+    "score": [85, 78, 92, 88, 95, 80, 89, 76, 84, 90,91]
 }
 
 df = pd.DataFrame(data)
