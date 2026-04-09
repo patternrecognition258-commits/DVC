c24c4262d337d5a0363a3dfaf85748f855d7e6f9
patternrecognition258 <patternrecognition258@gmail.com>
2026-04-10 01:46:50 +0530

    before DVC

---
+++ mycode.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+
+data = {
+    "name":  ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Helen", "Ian", "Jack"],
+    "age":   [25, 30, 22, 28, 26, 24, 27, 23, 29, 21],
+    "score": [85, 78, 92, 88, 95, 80, 89, 76, 84, 90]
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv('sample.csv')
