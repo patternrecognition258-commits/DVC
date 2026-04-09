36bc8e238bf20c9e7052416629ac9d3c36d31989
patternrecognition258 <patternrecognition258@gmail.com>
2026-04-10 02:26:48 +0530

    Data V3

--- mycode.py
+++ mycode.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 data = {
-    "name":  ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Helen", "Ian", "Jack","Sam"],
+    "name":  ["Alice", "Bob", "Charlie", "David", "Evan", "Frank", "Grace", "Helen", "Ian", "Jack","Sam"],
     "age":   [25, 30, 22, 28, 26, 24, 27, 23, 29, 21,23],
     "score": [85, 78, 92, 88, 95, 80, 89, 76, 84, 90,91]
 }
